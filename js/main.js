020e03a331f36d7458cb9f4c7c40ec747bfe9aac
Dildora Muslimova <85798640+faithpacifica@users.noreply.github.com>
2022-02-08 18:39:27 +0500

    form validation styles fix

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// const elSearchButton = document.querySelector('.search-nav-button');
+// const elSearchInput = document.querySelector('.search-nav-search');
+// const elSearchForm = document.querySelector('.search-nav-inner')
+
+// elSearchForm.addEventListener('click', (e) => {
+//     e.preventDefault();
+  
+//     if(e.target.value == '') {
+        
+//         elSearchInput.style.border = ' 3px solid #F46363';
+//         elSearchForm.classList.add('search-nav-inner--open');
+
+//     }
+    
+//     else (e.target.value != '') 
+//     {
+//         elSearchInput.style.border = ' 3px solid green';
+//         elSearchForm.classList.remove('search-nav-inner--open');
+//     }
+// })
